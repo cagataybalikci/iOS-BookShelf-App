2d505b8286cdb379d66c699108134b04b171fdd5
Çağatay Balıkçı <30704478+cagataybalikci@users.noreply.github.com>
2021-06-07 21:00:12 +0300

    Core Data implementation completed. TableView delete function added.

--- BookSelf/BookSelf/AddBookViewController.swift
+++ BookSelf/BookSelf/AddBookViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreData
 
 class AddBookViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
@@ -48,7 +49,43 @@ class AddBookViewController: UIViewController, UIImagePickerControllerDelegate, 
     //MARK: Save to Core Data
     
     @IBAction func saveDataBtnPressed(_ sender: Any) {
+        print("Button Tapped")
+        saveData()
+        
     }
     
-    
+    func saveData(){
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let books = NSEntityDescription.insertNewObject(forEntityName: "Books", into: context)
+        
+        books.setValue(bookTitleTextField.text!, forKey: "bookName")
+        
+        if let pageNumber = Int(pageNumberTextField.text!){
+            books.setValue(pageNumber, forKey: "pageNumber")
+        }
+        
+        
+       
+        books.setValue(authorTextField.text!, forKey: "author")
+        books.setValue(UUID(),forKey: "id")
+        
+        let data = bookImage.image!.jpegData(compressionQuality: 0.5)
+        
+        books.setValue(data, forKey: "image")
+        
+        
+        
+        do {
+            try context.save()
+            print("Data Saved")
+        } catch {
+            print("Error!")
+        }
+        
+        NotificationCenter.default.post(name: NSNotification.Name(rawValue: "dataEntered"), object: nil)
+        navigationController?.popViewController(animated: true)
+        
+    }
 }
--- BookSelf/BookSelf/ViewController.swift
+++ BookSelf/BookSelf/ViewController.swift
@@ -6,15 +6,17 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
     
-    var bookArray = [UIImage(named: "lostsymbol"),UIImage(named: "lepassager")]
-    var bookTitleArray = ["Lost Symbol","La Passager"]
-    var bookPageNumber = ["528","680"]
-    var authorArray = ["Dan Brown","Jean-Christophe Grangé"]
+    var bookIds = [UUID]()
+    var bookTitles = [String]()
+    var authors = [String]()
+    var bookCovers = [UIImage]()
+    var pageNumbers = [Int]()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -23,24 +25,130 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         // Navigation Controller
         navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addBook))
+        
+        getData()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        NotificationCenter.default.addObserver(self, selector: #selector(getData), name: NSNotification.Name(rawValue: "dataEntered"), object: nil)
+    }
     
+    //MARK: Segue To AddBookCV
     @objc func  addBook(){
         performSegue(withIdentifier: "toAddVC", sender: nil)
     }
     
+    //MARK: TableView Section
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "bookCell") as! BooksTableViewCell
-        cell.bookImage.image = bookArray[indexPath.row]
-        cell.bookTitleLabel.text = bookTitleArray[indexPath.row]
-        cell.pageNumberLabel.text = bookPageNumber[indexPath.row] + " pages"
-        cell.authorLabel.text = authorArray[indexPath.row]
+        if bookIds.count > 0 {
+            navigationController?.navigationBar.topItem?.title = "BookShelf"
+            cell.authorLabel.text = authors[indexPath.row]
+            cell.bookTitleLabel.text = bookTitles[indexPath.row]
+            cell.bookImage.image = bookCovers[indexPath.row]
+            cell.pageNumberLabel.text = String(pageNumbers[indexPath.row])
+        }else{
+            navigationController?.navigationBar.topItem?.title = "Add A Book"
+        }
         return cell
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return bookArray.count
+        return bookIds.count
+    }
+    
+    
+    // Delete from Core Data
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelegate.persistentContainer.viewContext
+            
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Books")
+            fetchRequest.predicate = NSPredicate(format: "id=%@", bookIds[indexPath.row].uuidString)
+            fetchRequest.returnsObjectsAsFaults = false
+            
+            do {
+                let results = try context.fetch(fetchRequest)
+                
+                if results.count > 0 {
+                    for result in results as! [NSManagedObject]{
+                        if let id = result.value(forKey: "id") as? UUID{
+                            if id == bookIds[indexPath.row] {
+                                context.delete(result)
+                                bookIds.remove(at: indexPath.row)
+                                bookTitles.remove(at: indexPath.row)
+                                authors.remove(at: indexPath.row)
+                                bookCovers.remove(at: indexPath.row)
+                                pageNumbers.remove(at: indexPath.row)
+                                self.tableView.reloadData()
+                                do {
+                                    try context.save()
+                                } catch {
+                                    print("Error")
+                                }
+                                break
+                            }
+                        }
+                    }
+                }
+            } catch {
+                print("Error")
+            }
+            
+        }
+        
+        
+        
+    }
+    
+    //MARK: Pull Data From Core Data
+    
+    @objc func getData(){
+        bookIds.removeAll(keepingCapacity: false)
+        bookTitles.removeAll(keepingCapacity: false)
+        authors.removeAll(keepingCapacity: false)
+        bookCovers.removeAll(keepingCapacity: false)
+        pageNumbers.removeAll(keepingCapacity: false)
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Books")
+        
+        fetchRequest.returnsObjectsAsFaults = true
+        
+        do {
+            let results = try context.fetch(fetchRequest)
+            
+            for result in results as! [NSManagedObject]{
+                if let booktitle = result.value(forKey: "bookName") as? String {
+                    bookTitles.append(booktitle)
+                }
+                
+                if let author = result.value(forKey: "author") as? String{
+                    authors.append(author)
+                }
+                
+                if let id = result.value(forKey: "id") as? UUID {
+                    bookIds.append(id)
+                }
+                
+                if let bookImage = result.value(forKey: "image") as? Data{
+                    let imageFromData = UIImage(data: bookImage)
+                    bookCovers.append(imageFromData!)
+                }
+                
+                if let pageNumber = result.value(forKey: "pageNumber") as? Int{
+                    pageNumbers.append(pageNumber)
+                }
+            }
+            tableView.reloadData()
+        } catch {
+            print("Error")
+        }
     }
 }
 
