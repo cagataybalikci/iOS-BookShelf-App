85994457224190c07963bdc49478bdc88f50cb88
Çağatay Balıkçı <30704478+cagataybalikci@users.noreply.github.com>
2021-07-01 09:06:10 +0300

    New UI. Project will use Firebase instead of Core Data.

---
+++ BookSelf/BookSelf/LoginViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LoginViewController.swift
+//  BookSelf
+//
+//  Created by Çağatay Balıkçı on 1.07.2021.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
