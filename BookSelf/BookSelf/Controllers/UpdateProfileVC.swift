9bcd0878f961ccf9fdcaf78b9e4b7449079fc3f2
Çağatay Balıkçı <30704478+cagataybalikci@users.noreply.github.com>
2021-08-16 20:53:51 +0300

    Settings VC added. UpdateProfileVC's Code refactored. UI imporvements made.

--- BookSelf/BookSelf/Controllers/UpdateProfileVC.swift
+++ BookSelf/BookSelf/Controllers/UpdateProfileVC.swift
@@ -10,26 +10,28 @@ import Firebase
 import SDWebImage
 
 class UpdateProfileVC: UIViewController,UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    
-    
-    
+
+    //MARK: PROPERTIES AND OUTLETS
     @IBOutlet weak var profileImageAddBtn: UIButton!
     @IBOutlet weak var BannerAddBtn: UIButton!
     @IBOutlet weak var profileImageView: UIImageView!
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var bioTextField: UITextField!
-    @IBOutlet weak var updateBtn: UIButton!
+    @IBOutlet weak var bannerImageView: UIImageView!
     
+    var buttonID = 0
     
+    //MARK: VIEW LIFE CYCLE
     override func viewDidLoad() {
         super.viewDidLoad()
-        let imageGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(selectImage))
-        profileImageView.addGestureRecognizer(imageGestureRecognizer)
-        profileImageView.isUserInteractionEnabled = true
-        updateBtn.isEnabled = false
         setupForGestureRecognizers()
+        buttonsUIConfig()
+    }
+    
+    //MARK: VIEW METHODS
+    //: CONFIG CODE FOR IMAGE PICK FUNCTION ~ ADD BORDER AND CORNER RADIUS
+     func buttonsUIConfig() {
         
-        //TODO: Extract Own Method
         profileImageAddBtn.layer.cornerRadius = profileImageAddBtn.bounds.width / 2
         profileImageAddBtn.layer.borderColor = UIColor(named: "CreatePostUIColor")?.cgColor
         profileImageAddBtn.layer.borderWidth = 2
@@ -39,13 +41,10 @@ class UpdateProfileVC: UIViewController,UIImagePickerControllerDelegate, UINavig
         profileImageView.layer.cornerRadius = profileImageView.bounds.width / 2
         
         BannerAddBtn.layer.cornerRadius = BannerAddBtn.bounds.width / 2
-        
     }
     
+    //MARK: DISMISS FUNCTIONS AND TAP GESTURE SETUP FOR TEXTFIELDS
     func setupForGestureRecognizers(){
-        let imageGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(selectImage))
-        profileImageView.addGestureRecognizer(imageGestureRecognizer)
-        profileImageView.isUserInteractionEnabled = true
         let dismissGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
         view.addGestureRecognizer(dismissGestureRecognizer)
     }
@@ -56,22 +55,27 @@ class UpdateProfileVC: UIViewController,UIImagePickerControllerDelegate, UINavig
     
     
     //MARK: IMAGEPICKER
-    @objc func selectImage(){
-        let picker = UIImagePickerController()
-        picker.delegate = self
-        picker.sourceType = .photoLibrary
-        picker.allowsEditing = true
-        present(picker, animated: true, completion: nil)
-    }
-    
+    func selectImage(){
+       let picker = UIImagePickerController()
+       picker.delegate = self
+       picker.sourceType = .photoLibrary
+       picker.allowsEditing = true
+       present(picker, animated: true, completion: nil)
+   }
+   
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+    if buttonID == 0{
         profileImageView.image = info[.originalImage] as? UIImage
         profileImageView.layer.cornerRadius = profileImageView.bounds.width / 2
-        updateBtn.isEnabled = true
-        self.dismiss(animated: true, completion: nil)
     }
+    else if buttonID == 1{
+        bannerImageView.image = info[.originalImage] as? UIImage
+    }
+       self.dismiss(animated: true, completion: nil)
+   }
     
-    //MARK: POST USER INFO TO DB
+    //MARK: POST USER INFO TO DB ~ CREATE FIRST TIME
+    // TODO: Seperate Update and Create
     func updateProfile(){
         if usernameTextField.text != "" && bioTextField.text != "" {
             if let image = profileImageView.image{
@@ -89,7 +93,7 @@ class UpdateProfileVC: UIViewController,UIImagePickerControllerDelegate, UINavig
                                     
                                     if let imageURL = imageURL{
                                         let fireStoreDB = Firestore.firestore()
-                                        let userInfo = ["profileImageURL":imageURL,"bio":self.bioTextField.text!,"username":self.usernameTextField.text!,"email":Auth.auth().currentUser?.email!] as [String:Any]
+                                        let userInfo = ["profileImageURL":imageURL,"bio":self.bioTextField.text!,"username":self.usernameTextField.text!,"email":Auth.auth().currentUser!.email!] as [String:Any]
                                         fireStoreDB.collection("Users").addDocument(data: userInfo) { Error in
                                             if Error != nil{
                                                 print(Error?.localizedDescription ?? "DB upload problem")
@@ -109,12 +113,22 @@ class UpdateProfileVC: UIViewController,UIImagePickerControllerDelegate, UINavig
         
     }
     
+    
+    //MARK: IBACTIONS
     @IBAction func cancelBtnPressed(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
     
-    @IBAction func updateBtnPressed(_ sender: Any) {
+    @IBAction func doneBtnPressed(_ sender: Any) {
         updateProfile()
     }
-        
+    
+    @IBAction func imageSelect(sender: UIButton){
+        if sender.tag == 0{
+            buttonID = 0
+        }else if sender.tag == 1{
+            buttonID = 1
+        }
+        selectImage()
+    }
 }
